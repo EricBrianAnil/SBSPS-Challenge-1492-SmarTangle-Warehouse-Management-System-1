1ad7922075c781cd87bc0cd3ed1b3dbcb862c404
Rahul Sunil <rahulsunil2@gmail.com>
2020-06-13 00:45:09 +0530

    Django App

--- stwms_django/stwms_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index,name="index"),
-    path('/home', views.home,name="home"),
-    path('accounts/sign_up/', views.sign_up, name="sign-up")
-]
