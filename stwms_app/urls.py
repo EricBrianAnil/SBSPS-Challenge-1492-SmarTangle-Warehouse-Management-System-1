4ee255feeb2eff0d11c8538165ed31754a05ac2a
Rahul Sunil <rahulsunil2@gmail.com>
2020-06-13 03:42:01 +0530

    /home - home

--- stwms_app/urls.py
+++ stwms_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index,name="index"),
-    path('/home', views.home,name="home"),
+    path('home', views.home,name="home"),
     path('accounts/sign_up/', views.sign_up, name="sign-up")
 ]
