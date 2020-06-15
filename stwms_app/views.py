8da39f7e6c5777cece0987a510eda36b80c05381
Rahul Sunil <rahulsunil2@gmail.com>
2020-06-15 14:35:23 +0530

    Added Favicon

--- stwms_app/views.py
+++ stwms_app/views.py
@@ -1,36 +1,42 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from rest_framework import serializers, viewsets, generics
+from rest_framework import viewsets, generics
 from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer
+
+
 # Create your views here.
 
 @login_required
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 
 def sign_up(request):
     context = {}
-    form = UserCreationForm(request.POST or None)
+    form = SignUpForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
             user = form.save()
-            login(request,user)
-            return render(request,'home.html')
-    context['form']=form
-    return render(request,'sign_up.html',context)
+            login(request, user)
+            return render(request, 'home.html')
+    context['form'] = form
+    return render(request, 'sign_up.html', context)
+
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)   
+    permission_classes = (AllowAny,)
