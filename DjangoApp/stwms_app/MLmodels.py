83f02499c320b4774f83810f81305bebdc4470a0
Rahul Sunil <rahulsunil2@gmail.com>
2020-06-29 16:15:23 +0530

    Updated ML model.

--- DjangoApp/stwms_app/MLmodels.py
+++ DjangoApp/stwms_app/MLmodels.py
@@ -6,21 +6,26 @@ from fbprophet import Prophet
 
 class TimeSeriesModel:
 
-    def __init__(self, file, item_index, split_percent):
+    def __init__(self, file='Online Retail.csv', item_index=False, future_period=30):
         self.df = pd.read_csv(file)
         self.item_index = item_index
-        self.split_percent = split_percent
+        self.prediction_size = future_period
         self.error_log = {}
 
-    def predictor(self):
+    def getData(self):
+        #TODO: Finish
+        pass
+
+    def data_clean(self):
         raw_df = self.df
         raw_df.Description = pd.Categorical(raw_df.Description)
         raw_df['item'] = raw_df.Description.cat.codes
-        raw_df['Date'] = pd.to_datetime(raw_df['Date'])
+        raw_df['Date_Time'] = pd.to_datetime(raw_df['Date_Time'])
         mod_df = raw_df.drop(['Description', 'Time', 'InvoiceNo'], axis=1)
-        mod_df.rename(columns={'Date': 'ds', 'Quantity': 'y'}, inplace=True)
+        mod_df.rename(columns={'Date_Time': 'ds', 'Quantity': 'y'}, inplace=True)
         pos_df = mod_df[mod_df['y'] > 0]
-        pos_df = pos_df[pos_df['item'] == self.item_index]
+        if self.item_index:
+            pos_df = pos_df[pos_df['item'] == self.item_index]
         final_df = pos_df.drop(['item'], axis=1)
         self.df = final_df.sort_values(by="ds")
 
@@ -31,35 +36,21 @@ class TimeSeriesModel:
         plt.ylabel('Quantity')
         plt.show()
 
-    def split(self):
-        prediction_size = int(self.split_percent * len(self.df))
-        train_df = self.df[:-prediction_size]
-        test_df = self.df[-prediction_size:]
-        return train_df, test_df, prediction_size
-
     def fb_prophet(self, plot=False):
-        self.predictor()
-        train_df, test_df, prediction_size = self.split()
-
+        self.data_clean()
         fbP_model = Prophet()
-        fbP_model.fit(train_df)
-        future = fbP_model.make_future_dataframe(periods=prediction_size)
+        fbP_model.fit(self.df)
+        future = fbP_model.make_future_dataframe(periods=self.prediction_size)
         forecast = fbP_model.predict(future)
 
-        y_hat = forecast['yhat'][-prediction_size:]
-        y = test_df['y']
+        y_hat = forecast['yhat'][-self.prediction_size:]
         y_hat.reset_index(drop=True, inplace=True)
-        y.reset_index(drop=True, inplace=True)
-
-        self.error_log['se'] = np.square(y_hat - y)
-        self.error_log['mse'] = np.mean(self.error_log['se'])
-        self.error_log['rmse'] = np.sqrt(self.error_log['mse'])
 
         if plot:
             plt.plot(forecast['ds'], forecast['yhat'])
-            plt.plot(train_df['ds'], train_df['y'], color='red')
+            plt.plot(self.df['ds'], self.df['y'], color='red')
             plt.show()
 
 
-model = TimeSeriesModel('datasets/Online Retail.csv', 15, 0.1)
+model = TimeSeriesModel('datasets/Online Retail.csv')
 model.fb_prophet(plot=True)
