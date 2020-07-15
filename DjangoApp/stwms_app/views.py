55242e879bd86b88f07e922acd43a8b29aae53be
Rahul Sunil <rahulsunil2@gmail.com>
2020-07-15 08:20:46 +0530

    URL Added

--- DjangoApp/stwms_app/views.py
+++ DjangoApp/stwms_app/views.py
@@ -41,7 +41,7 @@ except ValueError:
 
 
 def handler404(request, exception=None):
-    response = render("404.html")
+    response = render(request, "404.html")
     response.status_code = 404
     return response
 
